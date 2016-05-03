5ab25121b3576e30e686c5b6cb0c08242f6ef9a6
Ravi Jagannathan <ravijagannathan@Ravis-MacBook-Pro.local>
2016-05-03 16:13:07 -0400

    hh

--- 12_publishing_outputs/bump-timestamp-file.sh
+++ 12_publishing_outputs/bump-timestamp-file.sh
@@ -6,10 +6,11 @@ set -x # print commands
 git clone resource-gist updated-gist
 
 cd updated-gist
+ls -la
 echo $(date) > bumpme
 
 git config --global user.email "nobody@concourse.ci"
-git config --global user.name "Concourse"
+git config --global user.name "reagul"
 
 git add .
 git commit -m "Bumped date"
